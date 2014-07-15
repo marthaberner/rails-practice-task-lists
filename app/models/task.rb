b48ff4adbf3b0dbae7f3cf472f5e73be70f1665f
marthaberner <marthaberner.mb@gmail.com>
2014-07-15 17:36:31 -0600

    Tasks appear in order of their due date

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,8 @@ class Task < ActiveRecord::Base
   belongs_to :task_list
 
   validates :description, presence: {message: "Your task could not be created"}
+
+  def self.sort_by_due_date
+    all.sort_by { |task| task.due_date }
+  end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -6,4 +6,13 @@ describe Task do
 
     expect(task.save).to eq false
   end
+
+  it 'Orders tasks by due date, from soonest to latest' do
+    task1 = Task.create(due_date: "2014-07-18", description: "Due on the 18th")
+    task2 = Task.create(due_date: "2014-07-17", description: "Due on the 17th")
+
+    expected = [task2, task1]
+
+    expect(Task.sort_by_due_date).to eq(expected)
+  end
 end
