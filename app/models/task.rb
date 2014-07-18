44605c66b735661cfd2b5fdd10ee6f7e1d2560a0
marthaberner <marthaberner.mb@gmail.com>
2014-07-18 11:58:15 -0600

    Adjusted error message to not display field name

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ActiveRecord::Base
 
   belongs_to :task_list
 
-  validates :description, presence: {message: "Your task could not be created"}
+  validates_presence_of :description
 
   def self.sort_by_due_date
     all.sort_by { |task| task.due_date }
